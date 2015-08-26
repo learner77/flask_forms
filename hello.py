255486767dde6408276a122ceb30cd07ac2786cb
nex9 <nex9@core.pri>
2015-08-25 20:14:40 -0400

    login form

--- hello.py
+++ hello.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import Required
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'KnIp3#pPx%2'
 
-@app.route('/')
+class LoginForm(Form):
+    name = StringField('User Name', validators=[Required()])
+    passwd = PasswordField('Password', validators=[Required()])
+    submit = SubmitField('Login')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>your browser is: %s</p>' % user_agent
+    name = None
+    passwd = None
+    form = LoginForm()
+    return render_template('index.html', form=form, name=name, passwd=passwd)
 
 
 if __name__ == '__main__':
