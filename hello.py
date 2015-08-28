19fe38dbf9d06c90fa2de3a4a357b74e5b3a0af1
nex9 <nex9@core.pri>
2015-08-27 21:27:29 -0400

    username/password and host / alias forms

--- hello.py
+++ hello.py
@@ -1,27 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask import request
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
-from wtforms import PasswordField, StringField, SubmitField
+from wtforms import PasswordField, RadioField, SelectField, StringField, SubmitField
 from wtforms.validators import Required
-
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'KnIp3#pPx%2'
+dnsnames=[('hotel_primary','hotel_primary'),('air_primary','air_primary')]
+valid_aliases=[('ny-db-001','ny-db-001'),('ny-db-002','ny-db-002'),('ash1-db-401','ash1-db-401')]
 
 class LoginForm(Form):
     name = StringField('User Name', validators=[Required()])
     passwd = PasswordField('Password', validators=[Required()])
     submit = SubmitField('Login')
 
+class DnsHostForm(Form):
+    dnsname = SelectField('Host Name',choices=dnsnames)
+    change = SubmitField('Change')
+
+class DnsAliasForm(Form):
+    alias = RadioField('Alias Name',choices=valid_aliases)
+    change = SubmitField('Change')
+
+def validate_password(name, passwd):
+    if (name == "test") and (passwd == "winner"):
+        return True
+    else:
+        return False
+    
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     name = None
     passwd = None
     form = LoginForm()
-    return render_template('index.html', form=form, name=name, passwd=passwd)
+    if form.validate_on_submit():
+        if validate_password(form.name.data, form.passwd.data):
+            session['name'] = form.name.data
+            return redirect(url_for('dnshosts'))
+    return render_template('index.html', form=form, name=session.get('name'), passwd=passwd)
 
+@app.route('/dnshosts')
+def dnshosts():
+    dnsname = None
+    form = DnsHostForm()
+    return render_template('dnshost.html', form=form, dnsname=dnsname)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
